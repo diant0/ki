61435ba3fef872f8faa67897bbea0b9153760b40
diant <->
2024-03-30 02:28:58 +0500

    math: unit rectangle @Vector()

--- modules/math/src/rect.zig
+++ modules/math/src/rect.zig
@@ -13,3 +13,7 @@ pub inline fn rTopLeft(rect: anytype) @Vector(2, @typeInfo(@TypeOf(rect)).Vector
 pub inline fn rTopRight(rect: anytype) @Vector(2, @typeInfo(@TypeOf(rect)).Vector.child) {
     return .{ rect[0] + rect[2], rect[1] + rect[3] };
 }
+
+pub inline fn rUnit(T: type) @Vector(4, T) {
+    return .{ 0, 0, 1, 1 };
+}

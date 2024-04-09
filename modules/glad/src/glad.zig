0db5f931b8425f46d353c298fd53f50aa89f6f14
diant <->
2024-04-09 19:58:24 +0500

    glad: shorthand for clearing color and depth in same call

--- modules/glad/src/glad.zig
+++ modules/glad/src/glad.zig
@@ -8,6 +8,19 @@ pub fn load(getProcAddress: *const fn([*c]const u8) callconv(.C) ?*const fn() ca
     _ = glad.gladLoadGL(getProcAddress);
 }
 
+pub fn clear(clear_info: struct { col: ?@Vector(4, f32) = null, depth: ?f64 = null }) void {
+    var bitfield: glad.GLbitfield = 0;
+    if (clear_info.col) | col | {
+        bitfield |= glad.GL_COLOR_BUFFER_BIT;
+        glad.glClearColor(col[0], col[1], col[2], col[3]);
+    }
+    if (clear_info.depth) | depth | {
+        bitfield |= glad.GL_DEPTH_BUFFER_BIT;
+        glad.glClearDepth(depth);
+    }
+    glad.glClear(bitfield);
+}
+
 pub fn __enableDebugOutput() void {
 
     const printMsg = struct {

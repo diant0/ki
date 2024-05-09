b28843b49153b6f1405753873bc913e1cf5c887c
diant <->
2024-05-10 04:50:05 +0500

    glad: fixup impl.c and target/optimize parameters in build.zig

--- modules/glad/build.zig
+++ modules/glad/build.zig
@@ -2,14 +2,11 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
 
-    const target    = b.standardTargetOptions(.{});
-    const optimize  = b.standardOptimizeOption(.{});
-
     const lib = b.addStaticLibrary(.{
         .name               = "glad",
         .root_source_file   = b.path("src/glad.zig"),
-        .target             = target,
-        .optimize           = optimize,
+        .target             = b.standardTargetOptions(.{}),
+        .optimize           = b.standardOptimizeOption(.{}),
     });
 
     lib.linkLibC();
--- modules/glad/c/impl.c
+++ modules/glad/c/impl.c
@@ -1,2 +1,2 @@
 #define GLAD_GL_IMPLEMENTATION
-#include "glad.h"
+#include <glad.h>

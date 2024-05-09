f139e78717392d542478d4cecea425efe1728cb1
diant <->
2024-05-10 04:44:21 +0500

    glad: simplify build file by not generating implementation file, and not using separate module.

--- modules/glad/build.zig
+++ modules/glad/build.zig
@@ -2,19 +2,8 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
 
-    // --------------------------------
-
     const target    = b.standardTargetOptions(.{});
     const optimize  = b.standardOptimizeOption(.{});
-
-    // --------------------------------
-
-    const module = b.addModule("glad", .{
-        .root_source_file = b.path("src/glad.zig"),
-    });
-    module.addIncludePath(b.path("glad"));
-
-    // --------------------------------
 
     const lib = b.addStaticLibrary(.{
         .name               = "glad",
@@ -24,44 +13,10 @@ pub fn build(b: *std.Build) !void {
     });
 
     lib.linkLibC();
-    lib.addIncludePath(b.path("glad"));
 
-    try addGeneratedImpl(lib, "glad", "glad.c", "glad.h", "GLAD_GL_IMPLEMENTATION");
+    lib.addIncludePath(b.path("glad"));
+    lib.addCSourceFile(.{ .file = b.path("c/impl.c") });
 
     b.installArtifact(lib);
-
-    // --------------------------------
-
-}
-
-fn addGeneratedImpl(compile: *std.Build.Step.Compile, cache_subpath: []const u8, impl_filename: []const u8, header: []const u8, impl_define: []const u8) !void {
-
-    const b = compile.step.owner;
-
-    const cache_dir = b.cache_root.handle;
-
-    var impls_dir = try cache_dir.makeOpenPath(cache_subpath, .{});
-    defer impls_dir.close();
-    
-    const impl_file = impls_dir.openFile(impl_filename, .{}) catch | e | blk: {
-    
-        if (e == error.FileNotFound) {
-
-            const new_impl_file = try impls_dir.createFile(impl_filename, .{});
-            try new_impl_file.writer().print("#define {s}\n#include \"{s}\"\n", .{ impl_define, header });
-            break :blk new_impl_file;
-
-        } else return e;
-    
-    };
-    impl_file.close();
-
-    const impl_file_abspath = try impls_dir.realpathAlloc(b.allocator, impl_filename);
-    defer b.allocator.free(impl_file_abspath);
-
-    compile.addCSourceFile(.{
-        .file = .{ .cwd_relative = impl_file_abspath },
-        .flags = &[_][]const u8 {},
-    });
 
 }

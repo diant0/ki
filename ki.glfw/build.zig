de9d7fccc192ad91059994882dd423083a7edb70
diant0 <not@real.mail>
2025-11-13 23:58:02 +0500

    ki.glfw: build.zig now has vendor-glfw step

--- ki.glfw/build.zig
+++ ki.glfw/build.zig
@@ -9,19 +9,18 @@ pub fn build(b: *std.Build) void {
         .optimize = .Debug,
     }).artifact("exe");
 
-    const vendor_glfw_commit = b.option([]const u8, "vendor-glfw-commit", "specifies glfw commit to vendor, uses latest when unset");
+    const vendor_glfw_commit = b.option([]const u8, "vendor-glfw-commit", "specifies glfw commit to vendor");
     const run_vendor_git_repo_glfw = b.addRunArtifact(vendor_git_repo);
     run_vendor_git_repo_glfw.addArgs(&.{
         ".url=https://github.com/glfw/glfw",
         b.fmt(".dest={s}", .{b.pathResolve(&.{ b.build_root.path orelse ".", "vendor/glfw" })}),
-        // copy/filter lists will go here
         ".filters=+src,-src/CMakeLists.txt,+include,+deps/wayland,+LICENSE.md",
     });
-    if (vendor_glfw_commit) |commit| {
-        run_vendor_git_repo_glfw.addArg(b.fmt("commit={s}", .{commit}));
+    if (vendor_glfw_commit) |checkout_target| {
+        run_vendor_git_repo_glfw.addArg(b.fmt(".commit={s}", .{checkout_target}));
     }
 
-    const vendor_glfw = b.step("vendor-glfw", "updates glfw source, see -Dvendor-glfw-coomit option");
+    const vendor_glfw = b.step("vendor-glfw", "updates glfw source, see -Dvendor-glfw-commit option");
     vendor_glfw.dependOn(&run_vendor_git_repo_glfw.step);
 
     _ = target;
